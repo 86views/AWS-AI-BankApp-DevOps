fa3fd627a6d7fb7f383789d9bcfb015fe64c1f82
Oluleye Oluseun <oluseun.oluleye@gmail.com>
2026-09-24 17:43:47 +0100

    ci: allow idiomatic star imports for Spring/JPA packages

--- src/main/java/com/example/bankapp/model/Account.java
+++ src/main/java/com/example/bankapp/model/Account.java
@@ -1,6 +1,14 @@
 package com.example.bankapp.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Column;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.FetchType;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
